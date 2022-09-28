a48b7d00c5d5ee90d41b1fe97a9699e67839cd6b
Alexandre Mary <444deph12@gmail.com>
2022-09-28 00:48:37 -0700

    fixing errors

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void) 
 {
-	puts("\"Programmings like Building a multilingual puzzle");
+	puts("\"Programming is like Building a multilingual puzzle");
 	getchar();
 	return(0);
 }
