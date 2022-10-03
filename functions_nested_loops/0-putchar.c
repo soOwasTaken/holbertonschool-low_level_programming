fe4fd04c515fda6ef1858bbaca9c513063e67a2b
soOwasTaken <444deph12@gmail.com>
2022-10-03 10:27:40 +0000

    included some library

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
+#include "main.c"
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-  printf("\_putchar\n");
-  return 0;
+int main(void)
+{
+printf("\_putchar\n");
+return (0);
 }
