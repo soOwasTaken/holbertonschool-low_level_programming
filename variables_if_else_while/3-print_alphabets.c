ae6c27de4ce8f440ca5b9eb3308a4db42d720b21
soOwasTaken <444deph12@gmail.com>
2022-10-03 07:56:45 +0000

    add of task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch = 'a';
+char cha = 'A';
+char n = '\n';
+
+while (ch <= 'z' && cha <= 'Z')
+{
+if (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+while (ch > 'z' && cha <= 'Z')
+{
+putchar(cha);
+cha++;
+}
+putchar(n);
+}
+
+putchar(n);
+return (0);
+}
