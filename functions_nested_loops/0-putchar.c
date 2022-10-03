6ae1a5ce6b7b582e5a24d19442d08119a1ace1d5
soOwasTaken <444deph12@gmail.com>
2022-10-03 13:01:22 +0000

    .

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include "unistd.h"
 /**
  * main - Entry point
  *
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-	char c = '_putchar' ;
+	int c = '_putchar' ;
 
 	while (c)
 	{
