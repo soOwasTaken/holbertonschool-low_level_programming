1e74d20d163746356cea96f4e17a11898d8b1cf4
soOwasTaken <444deph12@gmail.com>
2022-10-03 06:56:48 +0000

    add of task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+for (int x = 'A'; x <= 'Z'; x++)
+{
+int lower_x = tolower(x);
+putchar(lower_x);
+}
+return (0);
+}
