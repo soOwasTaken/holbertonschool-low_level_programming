c7f2e1c8926c71206cbedc10aa25128e82109f16
soOwasTaken <444deph12@gmail.com>
2022-10-03 08:39:39 +0000

    add of task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char n = '\n';
+int a = 0;
+
+while (a <=  9)
+{
+printf("%d", a);
+a++;
+}
+putchar(n);
+return (0);
+}
