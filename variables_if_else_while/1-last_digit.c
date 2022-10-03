4e0aa90ce5911638c7a8aedda3b581448d389b05
soOwasTaken <444deph12@gmail.com>
2022-10-03 05:25:10 +0000

    changed place of lastDigit variable

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,9 +13,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-int lastDigit = n % 10;
 if (lastDigit > 5)
 {
+	int lastDigit = n % 10;
 printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else if (lastDigit < 6)
