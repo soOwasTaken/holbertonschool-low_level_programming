ea2e6cbdbfff1e3ff9bc3c3cc5bd85cc92eace7e
soOwasTaken <444deph12@gmail.com>
2022-10-04 10:12:14 +0000

    .

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	print_alphabet();
-	return (0);
+    print_alphabet();
+    return (0);
 }
