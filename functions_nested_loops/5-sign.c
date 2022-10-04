d1d61450d2a7ced977bbf361c2f010749eefb1e0
soOwasTaken <444deph12@gmail.com>
2022-10-04 18:59:58 +0000

    .

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -17,7 +17,6 @@ int print_sign(int n)
 	else if (n == 0)
 	{
 		return (0);
-		_putchar('0');
 		_putchar('\n');
 	}
 	else if (n < 0)
@@ -26,5 +25,5 @@ int print_sign(int n)
 		return (1);
 		_putchar('\n');
 	}
-return(0);
+return (0);
 }
