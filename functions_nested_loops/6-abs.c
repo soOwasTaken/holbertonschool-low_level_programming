6b48db996e5a6a0a54f87c3307e5da729da4c519
soOwasTaken <444deph12@gmail.com>
2022-10-04 19:13:42 +0000

    .

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_sign - call point
+ * @n: is the int checked
+ * Return: 1 if positive , 0 if zero , -1 if negative
+ */
+int _abs(int n)
+{
+   return n*((2*n+1)%2); 
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,3 +3,4 @@ int print_alphabet();
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
