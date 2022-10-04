c21ae2476543087276ce720c890389ec0c97ce59
soOwasTaken <444deph12@gmail.com>
2022-10-04 03:01:25 +0000

    .

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,8 +8,14 @@
  */
 int _putchar(char c)
 {
-	c = '\_putchar';
+	_putchar('_');
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
 	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
 	_putchar("\n");
 	return (write(1, &c, 1));
 }
