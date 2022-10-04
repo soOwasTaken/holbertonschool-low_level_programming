22c6d73f1d09e22a77bde9df3e949b5cc3aee5ba
soOwasTaken <444deph12@gmail.com>
2022-10-04 03:02:03 +0000

    .

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,13 +9,13 @@
 int _putchar(char c)
 {
 	_putchar('_');
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putchar(r);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar("\n");
 	return (write(1, &c, 1));
 }
