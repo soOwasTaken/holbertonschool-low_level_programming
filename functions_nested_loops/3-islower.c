161e65d6433ad47fc9a1ce8fc53cf263e653c370
soOwasTaken <444deph12@gmail.com>
2022-10-04 12:06:49 +0000

    .

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,13 +3,13 @@
 #include <ctype.h>
 #include "main.h"
 /**
- * print_alphabet - call point
+ * _islower - call point
  *
  * Return: Always 0 (Success)
  */
 int _islower(int c)
 {
 islower(c);
-return(0);
+return (0);
 }
 
