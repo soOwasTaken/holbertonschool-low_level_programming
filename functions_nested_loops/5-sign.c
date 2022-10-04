f1a3a03c37e5c6b0cce30ed2ca2797b51a338d46
soOwasTaken <444deph12@gmail.com>
2022-10-04 13:02:26 +0000

    .

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_sign - call point
+ * @n: is the int checked
+ * Return: 1 if positive , 0 if zero , -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+		_putchar('+');
+		_putchar('\n')
+	if (n == 0)
+		return(0)
+		_putchar('0')
+	if (n < 0)
+		return(-1)
+		_putchar('-')
+
+	return (0);
+}
+
