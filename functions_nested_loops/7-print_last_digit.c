1b24664dc5353ac65d56fe5acc96273739a25a0d
soOwasTaken <444deph12@gmail.com>
2022-10-04 19:21:21 +0000

    .

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * _abs - call point
+ * @n: is the int checked
+ * Return: absolute value
+ */
+int print_last_digit(int n)
+{
+return (n % 10);
+}
