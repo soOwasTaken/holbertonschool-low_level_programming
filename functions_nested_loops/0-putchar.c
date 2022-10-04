da3280ff755171a7e9470882af48c07f21676693
soOwasTaken <444deph12@gmail.com>
2022-10-04 03:06:01 +0000

    .

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,16 +6,8 @@
  *
  * Return: Always 0 (Success)
  */
-int _putchar(char c)
+int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar("\n");
-	return (write(1, &c, 1));
+	printf("\_putchar\n");
+	return (0);
 }
