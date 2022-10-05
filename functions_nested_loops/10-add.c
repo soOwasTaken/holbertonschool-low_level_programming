75c1d416a6d17462f86a45acacd9b59e6d628935
soOwasTaken <444deph12@gmail.com>
2022-10-05 11:18:44 +0000

    .

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * add - call point
+ * @a: first int
+ * @b: second int
+ * Return: addition of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
