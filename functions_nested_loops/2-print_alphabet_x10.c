73f7a19ee1b7088188c8a9c18fa2618624fb4a75
soOwasTaken <444deph12@gmail.com>
2022-10-05 11:12:36 +0000

    .

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,17 @@
  *
  * Return: void
  */
-int print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
-char c;
-int i;
-for (i = 0; i < 10; i++)
-{
-	for (c = 'a'; c <= 'z'; c++)
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
-	_putchar(c);
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
 }
