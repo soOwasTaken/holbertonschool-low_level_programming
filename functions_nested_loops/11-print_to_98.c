09ddbd3312b3a48ddfbbe43265a383f0edf2121d
soOwasTaken <444deph12@gmail.com>
2022-10-05 11:32:46 +0000

    .

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_to_98 - call point
+ * @n: int asked
+ * 
+ * Return: every natural numbers until it reach 98
+ */
+void print_to_98(int n)
+{	
+	while (n != 98)
+	{
+		printf("%i, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
+	}
+	printf("98\n");
+}
