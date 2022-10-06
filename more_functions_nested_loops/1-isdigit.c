f13e12cc1f554c4592ec8a425c3eb2610d967b27
soOwasTaken <444deph12@gmail.com>
2022-10-06 07:55:07 +0000

    add of task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * _isdigit - call point
+ * @c: is the int checked
+ * Return: 0 if c isn't a digit, 1 if he is
+ */
+int _isdigit(int c)
+{
+	if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+		return (0);
+	else
+		return (1);
+}
