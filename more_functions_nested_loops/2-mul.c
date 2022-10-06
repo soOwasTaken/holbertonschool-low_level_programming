39b1cca10e1d8106a82f75096dbaa6d0d7a78829
soOwasTaken <444deph12@gmail.com>
2022-10-06 08:06:55 +0000

    add of task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * mul - call point
+ * @a: first interger
+ * @b: second interger
+ * Return: return result
+ */
+int mul(int a, int b)
+{
+	return(a * b)
+}
