fe163251ba952f025540fe344dde2e6802c8a343
soOwasTaken <444deph12@gmail.com>
2022-10-06 08:37:01 +0000

    changed description for betty

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,14 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * mul - call point
- * @a: first interger
- * @b: second interger
- * Return: return result
- */
+* print_numbers - call point
+* i: first integrer
+* Return: return result
+*/
 void print_numbers(void)
 {
 int i = 0;
 for (i = 0; i <= 9; i++)
-    _putchar(i + '0');
+	_putchar(i + '0');
 _putchar('\n');
 }
