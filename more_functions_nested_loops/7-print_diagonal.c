73fd67beb2401406fdd1dcc3837f354246bca05f
soOwasTaken <444deph12@gmail.com>
2022-10-06 17:16:52 +0000

    add of task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+* print_diagonal - call point
+* @n: int required
+* Return: void
+*/
+void print_diagonal(int n)
+{
+int a, b;
+if (n <= 0)
+	putchar('\n');
+for (a = 0; a < n; a++)
+{
+	for (b = 0; b < a; b++)
+	{
+	_putchar(' ');
+	}
+_putchar('\\');
+_putchar('\n');
+}
