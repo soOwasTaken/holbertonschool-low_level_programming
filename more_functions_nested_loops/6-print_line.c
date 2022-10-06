f50ad644589a740389adc3e4cc6a23a19dfea6c5
soOwasTaken <444deph12@gmail.com>
2022-10-06 16:33:18 +0000

    add of task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+* print_line - call point
+*
+* Return: void
+*/
+void print_line(int n)
+{
+int n;
+while ((n > 0))
+{
+n--;
+putchar('_');
+}
+putchar('\n');
+}
