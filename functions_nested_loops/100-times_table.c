b669b014ff0a99b0768f20d8a9ed534b8254db93
soOwasTaken <444deph12@gmail.com>
2022-10-07 13:15:32 +0000

    .

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -10,6 +10,8 @@
 void print_times_table(int n)
 {
 int a, b, c, d, e, f;
+if (n >= 0 && n < 15)
+{
 for (a = 0; a < n; a++)
 {
 for (b = 0; b < n; b++)
@@ -45,6 +47,7 @@ _putchar(' ');
 }
 else
 _putchar('\n');
+}
 }
 }
 }
