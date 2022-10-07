27da69cc0ea713991447ffb08cf82c6535e81405
soOwasTaken <444deph12@gmail.com>
2022-10-07 08:31:52 +0000

    add of fizzbuzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - call point
+ *
+ * Return: FizzBuzz
+ */
+
+int main(void)
+{
+int n = 100;
+for (int i = 0; i <= n; i++)
+{
+if ((i % 5 == 0) && (i != 0))
+{
+printf("Buzz ");
+else if ((i % 3 == 0) && (i != 0))
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+}
