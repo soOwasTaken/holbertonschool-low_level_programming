08013d32638ae2a35d3114ef0749c9996c715329
soOwasTaken <444deph12@gmail.com>
2022-10-07 09:34:55 +0000

    changed small stuffs

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_triangle - call point
+ * @size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+int i, j;
+for (i = 1; i <= size; i++)
+{
+for (j = i; j < size; j++)
+{
+printf(" ");
+}
+for (j = 1; j <= i; j++)
+{
+printf("#");
+}
+printf("\n");
+}
+return (void);
+}
