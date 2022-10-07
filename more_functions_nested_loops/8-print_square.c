903c5e7c9eafb9db2202fda7816e1b3cc3c861cb
soOwasTaken <444deph12@gmail.com>
2022-10-07 07:20:47 +0000

    add of task with square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+* print_square - call point
+* @size: int required
+* Return: a square of @size
+*/
+void print_square(int size)
+{
+int a, b;
+if (n <= 0)
+	putchar('\n');
+for (a = 0; a < n; a++)
+{
+	for (b = 0; b < n; b++)
+	{
+	_putchar('#');
+	}
+_putchar('\n');
+}
+}
