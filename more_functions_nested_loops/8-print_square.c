3dec1410be246f5e033cbde8e1c6a5599f087d63
soOwasTaken <444deph12@gmail.com>
2022-10-07 07:25:00 +0000

    .

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -13,10 +13,10 @@ if (size <= 0)
 	putchar('\n');
 for (a = 0; a < size; a++)
 {
-	for (b = 0; b < size; b++)
-	{
-    _putchar('#');
-	}
+for (b = 0; b < size; b++)
+{
+_putchar('#');
+}
 _putchar('\n');
 }
 }
