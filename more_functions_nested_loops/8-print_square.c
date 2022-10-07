20b436ddc7184c04d6734f967a7233a63a02d46d
soOwasTaken <444deph12@gmail.com>
2022-10-07 07:21:48 +0000

    add of task with square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,13 +9,13 @@
 void print_square(int size)
 {
 int a, b;
-if (n <= 0)
+if (size <= 0)
 	putchar('\n');
-for (a = 0; a < n; a++)
+for (a = 0; a < size; a++)
 {
-	for (b = 0; b < n; b++)
+	for (b = 0; b < size; b++)
 	{
-	_putchar('#');
+	_putchar('#');  
 	}
 _putchar('\n');
 }
