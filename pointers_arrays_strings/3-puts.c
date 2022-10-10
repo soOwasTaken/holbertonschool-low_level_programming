dfee0a691ae9bfeb827f66394adf934a21883906
soOwasTaken <444deph12@gmail.com>
2022-10-10 09:19:41 +0000

    add of task 4

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * _puts - Entry point
+ * @str: string
+ * Return: void
+ */
+void _puts(char *str)
+{
+char *ptr = *str
+while(*ptr!=0)
+	putchar(*ptr++);
+}
