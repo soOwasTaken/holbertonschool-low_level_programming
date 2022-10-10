8e5a09317cf1868bd0a8547541428312e5516ee5
soOwasTaken <444deph12@gmail.com>
2022-10-10 08:09:18 +0000

    .

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * swap_int - Entry point
+ * @a: pointer a change value
+ * @b: pointer b change value
+ * Return: void
+ */
+int _strlen(char *s)
+{
+printf("%d\n",sizeof(char));
+}
