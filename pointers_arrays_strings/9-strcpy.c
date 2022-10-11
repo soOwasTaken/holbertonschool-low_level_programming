c41c72a636ec4dfc4d4b4932978528b1327758a7
soOwasTaken <444deph12@gmail.com>
2022-10-11 13:35:38 +0000

    adding task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *_strcpy - Entry point
+ * @dest: pointer of destination
+ * @src: pointer of source
+ * Return: void
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < src[i]; i++)
+		dest[i] = '\0';
+return (dest);
+}
