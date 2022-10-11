e5c70c76727d3289e48616c63a571701d111c13e
soOwasTaken <444deph12@gmail.com>
2022-10-11 12:51:09 +0000

    .

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - Entry point
+ * @str: string
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+int i;
+for(i = 0;i <= n ; i++)
+{
+	if (i != n)
+		printf("%d, ",a[i]);
+	if (i == n)
+		printf("%d",a[i]);
+}
+putchar('\n');
+}
