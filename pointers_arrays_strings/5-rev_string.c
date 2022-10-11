a2abed41873255ba3f3232066fcca29f7eb65f6a
soOwasTaken <444deph12@gmail.com>
2022-10-11 08:37:18 +0000

    add of task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -3,19 +3,19 @@
 #include <string.h>
 #include "main.h"
 /**
- * print_rev - Entry point
+ * rev_string - Entry point
  * @s: string
  * Return: void
  */
 void rev_string(char *s)
 {
-int i,j,len;
+int i, j, len;
 i = 0;
 len = strlen(s);
-for (i = 0; i < len / 2; i++) 
-{
-  j = s[i];
-  s[i] = s[len -i -1];
-  s[len -i -1] = j;
-}
+for (i = 0; i < len / 2; i++)
+	{
+	j = s[i];
+	s[i] = s[len - i - 1];
+	s[len - i - 1] = j;
+	}
 }
