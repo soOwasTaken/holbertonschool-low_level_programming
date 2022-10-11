0e6927930fce7a34e919c602ad7af95cdfbc3f2c
soOwasTaken <444deph12@gmail.com>
2022-10-11 13:52:53 +0000

    test

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,7 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; i <= src[i]; i++)
 		dest[i] = src[i];
-	for ( ; i < src[i]; i++)
-		dest[i] = '\0';
-	return (dest);
+return (dest);
 }
