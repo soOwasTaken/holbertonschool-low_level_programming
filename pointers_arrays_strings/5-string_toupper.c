f35b43bbd5beb811b85e55f7ce4101903a4e36a5
soOwasTaken <444deph12@gmail.com>
2022-10-12 16:43:25 +0000

    add of task 6

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+#include <string.h>
+/**
+ * string_toupper - Entry point
+ *
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int len = strlen(str);
+	int i;
+	
+	for (i = 0; i < len ; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 'a' + 'A' ;
+		}
+	}
+	return (str);
+}
