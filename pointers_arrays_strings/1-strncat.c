2ac272afd4f87f9688c63bec64e10380a099681d
soOwasTaken <444deph12@gmail.com>
2022-10-12 14:45:55 +0000

    add of task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - Entry point
+ * @dest: pointer of destination
+ * @src: pointer of source
+ * Return: void
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int dest_len = strlen(dest);
+	for (i = 0; i < n && src[i] != '\0' ; i++)
+		dest[dest_len + i] = src[i];
+	return (dest);
+}
