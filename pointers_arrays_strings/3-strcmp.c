a6ad6f89cd5189c10b49951c2307625478eaa8d5
soOwasTaken <444deph12@gmail.com>
2022-10-12 15:10:08 +0000

    add of task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,18 +3,17 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strncpy - Entry point
- * @dest: pointer of destination
- * @src: pointer of source
- * @n: int n
+ * _strcmp - Entry point
+ * @s1: pointer of destination
+ * @s2: pointer of source
  * Return: void
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+int i;
 
-    for (i = 0 ; *s1 == *s2; s1++, s2++, i++)
-		if (*s1 == '\0')
-	   	 return 0;
-    return (*(s1 + i) - *(s2 + i));
+for (i = 0 ; *s1 == *s2; s1++, s2++, i++)
+	if (*s1 == '\0')
+		return (0);
+return (*(s1 + i) - *(s2 + i));
 }
