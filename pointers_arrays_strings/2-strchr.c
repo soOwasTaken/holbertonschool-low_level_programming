8e4722742b8f1bd9cfd0527824b51ef2e2018c70
soOwasTaken <444deph12@gmail.com>
2022-10-13 13:42:55 +0000

    fixing syntax

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,12 @@
  */
 char *_strchr(char *s, char c)
 {
-   while(*s != c && *s != '\0') {
-      s++;
-   }
-   if(*s == c) {
-      return s;
-   }else {
-      return NULL;
-   }
+	while (*s != c && *s != '\0')
+	{
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return (NULL);
 }
