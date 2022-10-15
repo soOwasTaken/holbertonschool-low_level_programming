274331ef4ab15fa9a9310b210bc60695eb2dd50c
soOwasTaken <444deph12@gmail.com>
2022-10-15 16:12:37 +0000

    task9

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+#include <string.h>
+/**
+ * print_diagsums - Entry point
+ * @a:  array
+ * print addition of diagonals
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, p, l = 0, r = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
+}
