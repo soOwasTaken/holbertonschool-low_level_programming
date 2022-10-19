d8068d1529e21c4e2dbf5d94e59e4f78502f1cb4
soOwasTaken <444deph12@gmail.com>
2022-10-19 10:02:55 +0000

    add of task 3

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - call point
+ * @argc: argc
+ * @argv: argv
+ * Return: 1 if c is upper, 0 if he isn't
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i <= argc; i++)
+	{
+		argv[i]++;
+		if (i == argc - 1)
+			printf("%d\n", i);
+	}
+	return (0);
+}
