03b4b9d1807a4e01b243df6cbca8b14ec4b6e4f0
soOwasTaken <444deph12@gmail.com>
2022-10-19 09:43:37 +0000

    add of argc_argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - call point
+ * @argc: argc
+ * @argv: argv
+ * Return: 1 if c is upper, 0 if he isn't
+ */
+int main(int argc, char **argv)
+{
+	while(argc--)
+		printf("%s\n", *argv++);
+	exit(EXIT_SUCCESS);
+}
