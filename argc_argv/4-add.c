aed46619a44e8f447629ddc550eafa2eda3caded
soOwasTaken <444deph12@gmail.com>
2022-10-20 14:13:27 +0000

    add of task 5

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - additionnates numbers
+ * @argc: number of arguments
+ * @argv: argument vector
+ * Return: 0 for sucess, 1 for error
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
+}
