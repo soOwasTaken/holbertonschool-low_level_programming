2e8f912285cb03c40817094c04e984d940b887ef
soOwasTaken <444deph12@gmail.com>
2022-10-24 10:15:57 +0000

    adding files to start new week !

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * _strlen_recursion - Entry point
+ * @s: string to count
+ * Return: s
+ */
+char *create_array(unsigned int size, char c);
+{
+
+}
