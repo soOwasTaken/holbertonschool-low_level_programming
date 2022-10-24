395fc9ba3a57143109554dbcea18a9c2d5be78ea
Alexandre M <444deph12@gmail.com>
2022-10-24 12:17:26 +0000

    test of task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *_strdup - Entry point
+ *@str: string to duplicate
+ * Return: pointer to duplicated string
+ */
+char *_strdup(char *str)
+{
+	int str_size;
+	static char *dup;
+	char *dup_offset;
+
+	/* Allocate memory for duplicate */
+	org_size = strlen(org);
+	dup = (char *)malloc(sizeof(char)*str_size+1);
+	if( dup == NULL)
+		return( (char *)NULL);
+
+	/* Copy string */
+	dup_offset = dup;
+	while(*str)
+	{
+		*dup_offset = *str;
+		dup_offset++;
+		str++;
+	}
+	*dup_offset = '\0';
+
+	return(dup);
+}
