e9618afb256db3bd859135930979288743651278
soOwasTaken <444deph12@gmail.com>
2022-10-25 09:25:57 +0000

    changed s1 =  if null, same for s2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,6 +20,7 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL && s1 != NULL)
 		s2 = "";
+	new_str = malloc(s1_len + s2_len + 1);
 	if (new_str == NULL)
 		return (NULL);
 	memcpy(new_str, s1, s1_len);
