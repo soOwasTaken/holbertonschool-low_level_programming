4d2807b37cce270ce9e7e5cb61015a09d972c905
soOwasTaken <444deph12@gmail.com>
2022-10-25 12:54:53 +0000

    add of task 0 and project folder

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *malloc_checked - Entry point
+ *@b: integer b
+ * Return: exit
+ */
+void *malloc_checked(unsigned int b)
+{
+int *ptr ;
+
+ptr = (int *)malloc(sizeof(int b));
+if (ptr == NULL)
+    exit(98);
+exit(0);
+}
