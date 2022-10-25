7226e62929b0ac4d6a6258aaffe1f668c2a27edc
soOwasTaken <444deph12@gmail.com>
2022-10-25 09:50:21 +0000

    testing

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *str_concat - Entry point
+ *@s1: string1
+ *@s2: string2
+ * Return: new_str
+ */
+char *alloc_grid(int width, int height)
+{
+	int i;
+	int **ptr;
+
+	if (width <= 0)
+		return (NULL);
+	if (height <= 0)
+		return (NULL);
+	ptr = malloc(sizeof(int) * height);
+	if (ptr == NULL)
+		return(NULL);
+	i = 0;
+	while (i < height)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+		if (ptr[i] == NULL)
+			return (NULL);
+		i++;
+	}
+	return(ptr);
+}
