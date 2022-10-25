b19dd1226c8c9dfc0e994539344eee196c677986
soOwasTaken <444deph12@gmail.com>
2022-10-25 13:30:51 +0000

    fixing syntax

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 	s2_len = strlen(s2);
 	if ((s1 == NULL) && (s2 != NULL))
 		s1 = "";
-	if ((s2 == NULL) && (s1 != NULL))
+	if ((s1 != NULL) && (s2 == NULL))
 		s2 = "";
 	if ((s1 == NULL) && (s2 == NULL))
 		s1 = s2 = "";
