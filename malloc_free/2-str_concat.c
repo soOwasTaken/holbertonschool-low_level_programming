b656e49e32bc1f5129a8679994414cd273c887e0
soOwasTaken <444deph12@gmail.com>
2022-10-25 08:59:25 +0000

    testing some loops

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,13 +13,13 @@ char *str_concat(char *s1, char *s2)
 {
 	int s1_len, s2_len;
 	char *new_str;
-
+	
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
 	new_str = malloc(s1_len + s2_len + 1);
-	if (s2 == NULL)
-		s2_len = 0;
-	memcpy(new_str, s1, s1_len);
-	memcpy(new_str + s1_len, s2, s2_len + 1);
+	if (s1 != NULL)
+		memcpy(new_str, s1, s1_len);
+	if (s2 != NULL)
+		memcpy(new_str + s1_len, s2, s2_len + 1);
 	return (new_str);
 }
