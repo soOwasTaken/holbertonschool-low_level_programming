f1d8dfec0940feb445e0b44c86f53f6aa9a66183
soOwasTaken <444deph12@gmail.com>
2022-10-25 08:41:23 +0000

    adding null value when s = 0

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,9 +14,13 @@ char *str_concat(char *s1, char *s2)
 	int s1_len, s2_len;
 	char *new_str;
 
+	if (s1 == NULL)
+		i = 0;
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
 	new_str = malloc(s1_len + s2_len + 1);
+	if (s == NULL)
+		return (NULL);
 	memcpy(new_str, s1, s1_len);
 	memcpy(new_str + s1_len, s2, s2_len + 1);
 	return (new_str);
