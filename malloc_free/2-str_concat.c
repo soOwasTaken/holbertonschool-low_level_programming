950469aa9c991828fb96de56b61e39db8ac9e9da
soOwasTaken <444deph12@gmail.com>
2022-10-25 09:10:59 +0000

    forgot one loop

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,15 +13,17 @@ char *str_concat(char *s1, char *s2)
 {
 	int s1_len, s2_len;
 	char *new_str;
-	
+
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
 	if (s1 == NULL && s2 != NULL)
 		new_str = malloc(s2_len + 1);
 	if (s2 == NULL && s1 != NULL)
-		new_str = malloc(s1_len+ 1);
+		new_str = malloc(s1_len + 1);
 	if (s1 && s2 == NULL)
-		return(NULL);
+		return (NULL);
+	if s1 && s2 != NULL
+		new_str = malloc(s1_len + s2_len + 1);
 	memcpy(new_str, s1, s1_len);
 	memcpy(new_str + s1_len, s2, s2_len + 1);
 	return (new_str);
