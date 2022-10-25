14a0455ce19811deb3b31fdc2b0f9bb66417031c
soOwasTaken <444deph12@gmail.com>
2022-10-25 09:36:05 +0000

    changed s1 =  if null, same for s2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
-	if (s1 == NULL && s2 != NULL)
+	if ((s1 == NULL) && (s2 != NULL))
 		s1 = "";
-	if (s2 == NULL && s1 != NULL)
+	if ((s2 == NULL) && (s1 != NULL))
 		s2 = "";
 	if (s2 && s1 == NULL)
 	{
