a6fd4a871d3aab679c5d2aa864ab1e32a7a1c9de
soOwasTaken <444deph12@gmail.com>
2022-10-26 10:00:15 +0000

    trying for loop

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * *array_range - function that creates an array of integers.
+ * @min: min value of integer
+ * @max: max value of integer
+ * Return: void
+ */
+int *array_range(int min, int max)
+{
+	void *ptr;
+	int i = 0;
+
+	if (min > max)
+		return (NULL);
+	ptr = malloc(max + 1);
+	for (i = 0 ; min <= max ; i++)
+		ptr[i] = (min + 1)
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
