224d7d7f14b459448f5eaad07da84b9e159a8ba9
soOwasTaken <444deph12@gmail.com>
2022-10-31 08:53:47 +0000

    add of task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+ /**
+ * struct dog - information of the dog
+ * @name: Name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
