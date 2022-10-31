0b930c19709d2c65f530394a055c7e541c507131
soOwasTaken <444deph12@gmail.com>
2022-10-31 10:14:19 +0000

    testing task 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - init struct of dog
+ * @d: pointer to struct
+ *
+ * Description: dog description
+ * return: dog value
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name)
+			printf("Name: %s", d->name);
+		else
+			printf("Name: (nil)");
+		if (d->age)
+			printf("\nAge: %f", d->age);
+		else
+			printf("\nAge: (nil)");
+		if (d->owner)
+			printf("\nOwner: %s\n",  d->owner);
+		else
+		printf("Owner: (nil)");
+	}
+}
