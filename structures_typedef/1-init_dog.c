8a6a007a417d10233b539b7fe3cb537bd372066a
soOwasTaken <444deph12@gmail.com>
2022-10-31 09:24:12 +0000

    add of task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <dog.h>
+/**
+ * init_dog - init struct of dog
+ * @d: pointer to struct
+ * @name: Name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: dog description
+ * return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name
+		d->age = age;
+		d->owner = owner;
+	}
+}
