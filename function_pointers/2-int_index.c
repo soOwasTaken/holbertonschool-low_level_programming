fdbe3b8c2c53fb6d2c35579c16287de74b07947f
soOwasTaken <444deph12@gmail.com>
2022-11-02 17:42:28 +0000

    fixing betty and description

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,21 +1,21 @@
 #include "function_pointers.h"
 /**
- * array_iterator - function with 3 parameter + a function
+ * int_index - function that searches for an integer.
  * @array: array
  * @size: size of array
- * @action: function used on the array
+ * @cmp: function used on the array
  * Description: use a function on a array
- * Return: void
+ * Return: matching result of cmp with the array
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (array && cmp && size > 1)
-		{
-			for (i = 0; i <= size; i++)
-				if (cmp(array[i]))
-					return (i);
-		}
-	return(-1);
+	{
+	for (i = 0; i <= size; i++)
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
 }
