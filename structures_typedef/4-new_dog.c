bd6a5de528a8f8775f46b10ea78a2c33f4044f12
soOwasTaken <444deph12@gmail.com>
2022-11-02 03:35:11 +0000

    trying memory leak

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -40,8 +40,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog->owner == NULL)
 		return (NULL);
 	strcpy(new_dog->owner, owner);
-	if (new_dog == NULL)
-		free(new_dog);
-        return (NULL);
 	return (new_dog);
 }
