3acc5f91f3b8c57bcd96e048127106dcf6b82694
soOwasTaken <444deph12@gmail.com>
2022-11-02 14:24:48 +0000

    test

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,6 +15,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 
 	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 	new_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (new_dog->name == NULL)
 	{
@@ -24,21 +29,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	strcpy(new_dog->name, name);
 
 	new_dog->age = age;
+	new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (owner == NULL)
 	{
-		free(new_dog);
 		free(new_dog->owner);
+		free(new_dog);
 		return (NULL);
 	}
-	new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-
-	if (new_dog == NULL)
-	{
-		free(new_dog);
-		free(new_dog->owner);
-		free(new_dog->name);
-	}
-
 	strcpy(new_dog->owner, owner);
 	return (new_dog);
 }
