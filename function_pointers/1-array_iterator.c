cf445726a266eb5c8cd8233637ec7ddbfbeaf6fd
soOwasTaken <444deph12@gmail.com>
2022-11-02 10:32:57 +0000

    add of task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -6,7 +6,7 @@
  * Description: print name
  * Return: void
  */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	for (i = 0, i < size; i++)
 		action(array[i]);
