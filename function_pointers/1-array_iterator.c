342c7d046b29add582dcf123273671aa29ee0ea5
soOwasTaken <444deph12@gmail.com>
2022-11-02 10:39:08 +0000

    changed i to unsigned, and fixing betty stuff

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,14 +1,19 @@
 #include "function_pointers.h"
 /**
- * print_name - function with two arguments printing name
- * @name: char name that should be printed
- * @f: pointer
- * Description: print name
+ * array_iterator - function with 2 parameter + a function
+ * @array: i mean it's a array
+ * @size: size of array
+ * @action: function used on the array
+ * Description: use a function on a array
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    unsigned int i;
-	for (i = 0; i < size; i++)
+	unsigned int i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
 		action(array[i]);
+	}
 }
