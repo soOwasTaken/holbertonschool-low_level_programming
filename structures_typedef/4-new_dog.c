2269f602e32fd1629c01089a86cb97411751e0d2
soOwasTaken <444deph12@gmail.com>
2022-11-02 14:43:40 +0000

    testing changing frees

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,11 +15,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 
 	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
 	new_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (new_dog->name == NULL)
 	{
@@ -31,6 +26,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->age = age;
 	new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (owner == NULL)
+	{
+		free(owner);
+		free(new_dog);
+		return (NULL);
+	}
+	if (new_dog == NULL)
 	{
 		free(new_dog->owner);
 		free(new_dog);
