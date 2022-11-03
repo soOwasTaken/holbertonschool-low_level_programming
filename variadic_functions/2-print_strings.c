1c5deb8ae6b54ca5ca0df389f02d6ce072fb0294
soOwasTaken <444deph12@gmail.com>
2022-11-03 05:52:26 +0000

    added (nil) if str is null

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -15,6 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		const char *str = va_arg(ap, const char*);
+
 		if (str != NULL)
 			printf("%s", str);
 		else
