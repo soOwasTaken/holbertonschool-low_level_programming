d51f5c813f54a6dbe209d29e1de0ad63f7b60fdd
soOwasTaken <444deph12@gmail.com>
2022-11-03 14:38:31 +0000

    add of last task

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -41,7 +41,7 @@ int op_div(int a, int b)
 {
     if (b == 0)
         printf("Error\n");
-        exit(100)
+        exit(100);
 	return (a / b);
 }
 /**
@@ -54,6 +54,6 @@ int op_mod(int a, int b)
 {
     if (b == 0)
         printf("Error\n");
-        exit(100)
+        exit(100);
 	return (a % b);
 }
