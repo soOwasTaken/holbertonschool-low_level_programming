427f82f6659e0a1ed43741e82e23946c1c37aab6
soOwasTaken <444deph12@gmail.com>
2022-11-03 03:49:37 +0000

    sum_them_all

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * sum_them_all - Entry point
+ *@n: number of parameters
+ * Return: result of sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int res;
+
+	va_start(ap, arg1);
+	for (i = 0, res = 0 ; i < n ; i++)
+		res += va_arg(ap, int);
+	va_end(ap)
+return (res);
+}
