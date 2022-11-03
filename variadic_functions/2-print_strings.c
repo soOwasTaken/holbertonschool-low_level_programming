4c2f0099d3da5f0f5758e621bd633b5982916cdc
soOwasTaken <444deph12@gmail.com>
2022-11-03 05:48:12 +0000

    add of task 2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <string.h>
+/**
+ * print_strings - Entry point
+ *@n: number of parameters
+ *@separator: separator used between outputs.
+ * Return: printings strings with separator between
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		const char *str = va_arg( ap, const char* );
+        printf( "%s", str );
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+	}
+	va_end(ap);
+	putchar('\n');
+}
