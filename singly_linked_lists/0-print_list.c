a7d7ef27fad1b732055e2f203f368a7159468fba
Alexandre Mary <444deph12@gmail.com>
2022-11-14 14:57:42 +0000

    add of singly_linked_lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - call point
+ * @h: head of node
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
