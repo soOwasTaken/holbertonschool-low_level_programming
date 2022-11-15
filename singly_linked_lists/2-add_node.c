e54f51fa8f178a629bf4b9928feb263f351a1137
soOwasTaken <444deph12@gmail.com>
2022-11-15 05:53:40 +0000

    add of new_node->len

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,16 +9,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
     /* 1. allocate node */
-    list_t* new_node = (list_t*)malloc(sizeof(list_t));
+    list_t *new_node = malloc(sizeof(*new_node));
 	if(new_node == NULL)
 	{
 		free(new_node);
 	}
     /* 2. put in the data  */
     new_node->str  = strdup(str);
+	new_node->len = strlen(str);
     /* 3. Make next of new node as head */
     new_node->next = (*head);
     /* 4. move the head to point to the new node */
     (*head)= new_node;
-	return(new_node);
+	return(*head);
 }
