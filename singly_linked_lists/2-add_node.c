d40e1a2ec5e8a1f11f44320473f968c942963bc0
soOwasTaken <444deph12@gmail.com>
2022-11-15 06:07:28 +0000

    fixing betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,24 +2,25 @@
 
 /**
  * add_node - call point
- * @h: head of node
+ * @head: head of node
  * @str: str added node
  * Return: adress of node
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    /* 1. allocate node */
-    list_t *new_node = malloc(sizeof(*new_node));
-	if(new_node == NULL)
-	{
-		return(NULL);
-	}
-    /* 2. put in the data  */
-    new_node->str  = strdup(str);
+	/* 1. allocate node */
+	list_t *new_node = malloc(sizeof(*new_node));
+
+	if (new_node == NULL)
+		{
+		return (NULL);
+		}
+	/* 2. put in the data  */
+	new_node->str  = strdup(str);
 	new_node->len = strlen(str);
-    /* 3. Make next of new node as head */
-    new_node->next = (*head);
-    /* 4. move the head to point to the new node */
-    (*head)= new_node;
-	return(*head);
+	/* 3. Make next of new node as head */
+	new_node->next = (*head);
+	/* 4. move the head to point to the new node */
+	(*head) = new_node;
+	return (*head);
 }
