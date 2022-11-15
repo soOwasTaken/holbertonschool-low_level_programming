e6d5dab575219524b3c6f0b8c1f7af32f699fdd1
soOwasTaken <444deph12@gmail.com>
2022-11-15 06:03:22 +0000

    fixing betty and malloc

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,26 +2,24 @@
 
 /**
  * add_node - call point
- * @head: head of node
+ * @h: head of node
  * @str: str added node
  * Return: adress of node
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	/* 1. allocate node */
-	list_t *new_node = malloc(sizeof(*new_node));
-
-	if (*head == NULL || str == NULL)
+    /* 1. allocate node */
+    list_t *new_node = malloc(sizeof(*new_node));
+	if(new_node == NULL)
 	{
-		free(new_node);
-		return (NULL);
+		return(NULL);
 	}
-	/* 2. put in the data  */
-	new_node->str  = strdup(str);
+    /* 2. put in the data  */
+    new_node->str  = strdup(str);
 	new_node->len = strlen(str);
-	/* 3. Make next of new node as head */
-	new_node->next = (*head);
-	/* 4. move the head to point to the new node */
-	(*head) = new_node;
-	return (*head);
+    /* 3. Make next of new node as head */
+    new_node->next = (*head);
+    /* 4. move the head to point to the new node */
+    (*head)= new_node;
+	return(*head);
 }
