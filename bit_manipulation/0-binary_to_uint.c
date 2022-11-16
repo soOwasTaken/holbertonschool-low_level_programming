258910dcb1acb18ea8429f8cc90c0d485b0be964
soOwasTaken <444deph12@gmail.com>
2022-11-16 17:19:40 +0000

    adding a strlen

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,22 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int val = 0;
 	int i = 0;
+	int size = strlen(b);
 
 	if (b == NULL)
-	return (0);
+		return (0);
+	
 
-	while (b[i] == '0' || b[i] == '1')
+	while (i < size)
 	{	/* Found another digit.*/
-		val <<= 1;
-		val += b[i] - '0';
-		i++;
+		if (b[i] == '0' || b[i] == '1')
+		{
+			val <<= 1;
+			val += b[i] - '0';
+			i++;
+		}
+		else
+			return(0);
 	}
 	return (val);
 }
