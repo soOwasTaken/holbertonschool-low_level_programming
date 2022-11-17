0f5ae11156ddc72dba97843500c7b9fa2549c8a0
soOwasTaken <444deph12@gmail.com>
2022-11-17 10:57:04 +0000

    deleted del int

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -9,13 +9,11 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp;
-	listint_t *del;/* del pointer points to the node */
 	unsigned int i;
 
 	if (!head || !*head)
 		return (-1);
 	temp = *head;
-	del = temp->next;
 	if (index == 0)
 	{
 		*head = (*head)->next; /* Advancing the head pointer */
@@ -26,13 +24,13 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		for (i = 0; i < index - 1; i++)
 		{
-			if (temp == NULL)
-				return (-1);
-			temp = temp->next;
+		if (temp->next == NULL)
+			return (-1);
+		temp = temp->next;
 		}
 	}
-	temp->next = temp->next->next;
-	del->next = NULL;
-	free(del); /* Node is deleted */
+	temp = temp->next;
+	temp->next = temp->next;
+	free(temp); /* Node is deleted */
 	return (1);
 }
