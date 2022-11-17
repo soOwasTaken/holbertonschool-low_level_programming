0ecd5c41b0a9257f64ac1d6ac6424abc369ae9bd
soOwasTaken <444deph12@gmail.com>
2022-11-17 10:41:27 +0000

    putting some line out of count loop

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -29,10 +29,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			temp = temp->next;
 			if (temp == NULL)
 				return(-1);
-			temp->next = temp->next->next;
-			del->next = NULL;
-			free(del); /* Node is deleted */
 		}
+		temp->next = temp->next->next;
+		del->next = NULL;
+		free(del); /* Node is deleted */
 	}
 	return(1);
 }
