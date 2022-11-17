cd904c65f8063d183d591260de386807274999ad
soOwasTaken <444deph12@gmail.com>
2022-11-17 09:21:16 +0000

    fixing small misswrite

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return (NULL);
+		return (new_node);
 	}
 
 	for (i = 0; i < idx - 1; i++)
