25a8b79197cf32d026f1e51dfa93f9ca5a0671fa
soOwasTaken <444deph12@gmail.com>
2022-11-17 09:19:25 +0000

    add of task 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new_node->n = n;
 
+	if (tmp == NULL && idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (NULL);
+	}
+
 	for (i = 0; i < idx - 1; i++)
 	{
 		temp = temp->next;
