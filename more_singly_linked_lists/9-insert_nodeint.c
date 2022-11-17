94b7bac21d02e411dc757da276f9f0d9693a62c4
soOwasTaken <444deph12@gmail.com>
2022-11-17 09:19:56 +0000

    fixing small misswrite

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new_node->n = n;
 
-	if (tmp == NULL && idx == 0)
+	if (temp == NULL && idx == 0)
 	{
 		new->next = *head;
 		*head = new;
