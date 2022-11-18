87826950e28a9689c51b15c76c27825084b69a7b
soOwasTaken <444deph12@gmail.com>
2022-11-18 14:40:13 +0000

    wrong file name

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to
+ * flip to get from one number to another
+ *@n: number
+ *@m: second number
+ *
+ * Return: number of bits to flip from n to m
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int num = 0;
+	int i;
+
+	for (i = 0; i < 32; i++)
+	{
+		if (((n >> i) & 1) != ((m >> i) & 1))
+		{
+			num++;
+		}
+	}
+	if (m == 0)
+		return (2 * num);
+	else
+		return (num);
+}
