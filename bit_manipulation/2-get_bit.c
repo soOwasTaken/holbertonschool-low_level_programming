bff26f9876a395260506f4fc37a81d40b96bd302
soOwasTaken <444deph12@gmail.com>
2022-11-18 10:10:50 +0000

    add of task 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 		int res = (n >> index) & 1;
 
-		if ((res == '0') | (res == '1'))
+		if ((res == 0) || (res == 1))
 			return(res);
 		else
 			return(-1);
