fdcc322a0aa2ca57d33acb4c81d132e4e1f22c7a
soOwasTaken <444deph12@gmail.com>
2022-11-18 11:33:01 +0000

    add of task 4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * set_bit - call point
+ * @n: number to get index from
+ * @index: index
+ * Return: number with bit changed
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index < 32)
+		*n = ~(1 << index) & *n;
+	if (index > 32)
+		return (-1);
+	return (1);
+}
