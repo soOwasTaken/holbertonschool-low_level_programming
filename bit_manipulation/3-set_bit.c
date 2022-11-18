4d9808f8d1c7e3b89bb39a754440f531e0f51693
soOwasTaken <444deph12@gmail.com>
2022-11-18 10:32:13 +0000

    add of task 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * set_bit - call point
+ * @n: number to get index from
+ * @index: index
+ * Return: number with bit changed
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	*n = *n | (1 << index);
+	if (index > 32)
+		return (-1);
+	return (*n);
+}
