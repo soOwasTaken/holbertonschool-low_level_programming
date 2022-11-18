6b70673f7a755a78c06db657ba2f6e52d70d69a2
soOwasTaken <444deph12@gmail.com>
2022-11-18 10:03:16 +0000

    add of task 2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_bit - call point
+ * @n: number to get index from
+ * @index: index
+ * Return: 1 or 0
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int res = (n >> index) & 1
+	return(res);
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -4,4 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 #endif
