5b79977735bc3eb766888947c191fcaf1c80c18e
soOwasTaken <444deph12@gmail.com>
2022-11-18 10:09:53 +0000

    add of task 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * get_bit - call point
  * @n: number to get index from
@@ -9,6 +8,10 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int res = (n >> index) & 1;
-	return(res);
+		int res = (n >> index) & 1;
+
+		if ((res == '0') | (res == '1'))
+			return(res);
+		else
+			return(-1);
 }
