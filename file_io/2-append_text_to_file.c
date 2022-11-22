a9642ad8bb9c9de935bfc1c646cf4440ce9c968c
soOwasTaken <444deph12@gmail.com>
2022-11-22 12:33:32 +0000

    fixing non needed

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,30 +8,26 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd,wr, i = 0;
+	int i = 0, fd;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_APPEND);
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[i])
-		i++;
+	write(fd, text_content, i);
 
-
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (1);
-	}
-	else
-	{
-		wr=	write(fd, text_content, i);
-	}
-	if (wr == -1)
-		return(-1);
-	close(fd);
 	return (1);
 }
