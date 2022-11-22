fbe13bb37ac8a2b279b4a6a20c52d776445f5fc2
soOwasTaken <444deph12@gmail.com>
2022-11-22 13:45:48 +0000

    thinking simpler

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -9,12 +9,10 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp = head;
 
-	if (head == temp)
-		head = temp->next;
-	if (temp->next != NULL)
-		temp->next->prev = temp->prev;
-
-	if (temp->prev != NULL)
-		temp->prev->next = temp->next;
-	free(temp);
+	while(head)
+	{
+		head = head->next;
+		free(temp);
+		temp = head;
+	}
 }
