447e57daa00e603ab45c42055cf1c670da8f685c
soOwasTaken <444deph12@gmail.com>
2022-11-22 13:03:17 +0000

    add of doubly linked lists

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,9 +11,9 @@ size_t print_dlistint(const dlistint_t *h)
 	while (h != NULL)
 	{
 		if (h->n == 0)
-			printf("[0] (nil)\n");
+			printf("0\n");
 		else
-			printf("[%d]\n", h->n);
+			printf("%d\n", h->n);
 		i++;
 		h = h->next;
 	}
