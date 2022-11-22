183bc57329dcba0db4a0f744930f97745d71e661
soOwasTaken <444deph12@gmail.com>
2022-11-22 13:50:57 +0000

    add of task 5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - call point
+ * @head: pointer to pointer to head of node
+ * @index: index
+ * Return: return success
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int n = 0;
+
+	while (head != NULL)
+	{
+		if (n == index)
+			return (head);
+		head = head->next;
+		n++;
+	}
+	return (NULL);
+}
