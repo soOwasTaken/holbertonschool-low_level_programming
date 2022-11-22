a009f952935c1298485b4674c40b90952a0d6c03
soOwasTaken <444deph12@gmail.com>
2022-11-22 13:28:00 +0000

    add of task 3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,16 +1,20 @@
 #include "lists.h"
 
 /**
- * add_node_end - call point
+ * add_dnodeint_end - call point
  * @head: head of node
- * @str: str added node
+ * @n: n added node
  * Return: adress of last node or NULL if it fail
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(*new_node));
-	dlistint_t *last = *head; /* used in step 5*/
+	dlistint_t *last = *head;
 
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	new_node->n = n;
 	new_node->next = NULL;
 	if (*head == NULL)
