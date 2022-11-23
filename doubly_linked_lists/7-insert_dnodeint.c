69f0af110573dbf91e1abb6ab57b16a44687d33e
soOwasTaken <444deph12@gmail.com>
2022-11-23 08:48:37 +0000

    fixing betty

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,40 +2,43 @@
 
 /**
  * insert_dnodeint_at_index - call point
- * @: pointer to pointer to head of node
+ * @h: pointer to pointer to head of node
  * @idx: index
  * @n: data to add to the node
  * Return: return success
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = malloc(sizeof(*new_node));
-	dlistint_t *temp = *h;
-	unsigned int i;
+	dlistint_t *new_node, *temp;
 
-	if (new_node == NULL)
-	{
+	if (h == NULL)
 		return (NULL);
-	}
-	new_node->n = n;
 
 	if (idx == 0)
-	{
-		new_node->next = *h;
-		new_node->prev = NULL;
-		*h = new_node;
-		return (new_node);
-	}
-	for (i = 0; i < idx - 1; i++)
+		return (add_dnodeint(h, n));
+
+
+	temp = *h;
+
+	while (idx > 1 && temp && temp->next)
 	{
 		temp = temp->next;
-		if (temp == NULL)
-			return (NULL);
+		idx--;
 	}
-	new_node->next = temp->next;
+
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+		return (NULL);
+
+	if (idx > 1 || temp == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
 	if (temp->next != NULL)
-		new_node->next->prev = new_node;
-	temp->next = new_node;
+		temp->next->prev = new_node;
 	new_node->prev = temp;
+	new_node->next = temp->next;
+	temp->next = new_node;
 	return (new_node);
 }
