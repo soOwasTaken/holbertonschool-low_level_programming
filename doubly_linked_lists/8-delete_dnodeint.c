381d9bc66a784c750920f2f1311cb70af17fe796
soOwasTaken <444deph12@gmail.com>
2022-11-23 08:52:51 +0000

    add of task 8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - call point
+ * @head: pointer to pointer to head of node
+ * @index: index
+ * Return: return success
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp;
+	dlistint_t *del;/* del pointer points to the node */
+	unsigned int i;
+
+	if (!head || !*head)
+		return (-1);
+	del = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next; /* Advancing the head pointer */
+		free(del); /* Node is deleted */
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < index - 1; i++)
+		{
+		if (del->next == NULL)
+			return (-1);
+		del = del->next;
+		}
+	}
+	temp = del->next;
+	del->next = temp->next;
+	free(temp); /* Node is deleted */
+	return (1);
+}
