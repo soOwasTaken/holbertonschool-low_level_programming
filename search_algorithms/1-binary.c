25967fa927ea4ecbf0bc2ae4ccafd47b112eccd6
soOwasTaken <444deph12@gmail.com>
2023-01-02 10:35:10 +0000

    fix betty and add 1-binary.c

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -21,7 +21,7 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 		for (i = low; i <= high; i++)
 		{
-		printf("%d ", array[i]);
+		printf("%d, ", array[i]);
 		}
 	printf("\n");
 	mid = (low + high) / 2;
